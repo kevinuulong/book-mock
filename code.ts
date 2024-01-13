5d038592615632465f3d49158bf9e793ed518040
Kevin Long <kevin.uu.long@gmail.com>
2024-01-13 17:41:35 -0500

    Search Page UI
    Added placeholder UI for the Search/Search Results page

--- code.ts
+++ code.ts
@@ -7,7 +7,7 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { height: 360, themeColors: true });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -20,7 +20,7 @@ figma.ui.onmessage = msg => {
     for (let i = 0; i < msg.count; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
