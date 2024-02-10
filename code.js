d3e83c16af28fd71b6c6cade4025fe26cf74726f
Kevin Long <kevin.uu.long@gmail.com>
2024-02-10 12:21:58 -0500

    Results Page UI
    Add a placeholder UI for the results page.

--- code.js
+++ code.js
@@ -6,7 +6,7 @@
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { height: 360, themeColors: true });
+figma.showUI(__uiFiles__.result, { height: 360, themeColors: true });
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
